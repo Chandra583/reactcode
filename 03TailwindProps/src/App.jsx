ef6b277bdddec3d72f259a998a75320ce42c853e
Chandra583 <chandrashekhargawda2000@gmail.com>
2023-12-07 02:49:11 +0530

    TailwindProps

---
+++ 03TailwindProps/src/App.jsx
@@ -0,0 +1,26 @@
+// import { useState } from 'react'
+
+import './App.css'
+import Card from './Component/Card'
+
+function App() {
+
+  // let newObj = {
+  //   name: "John",
+  //   age: 30,
+  //   city: "New York"
+  // }
+
+  // let newArr = [1, 2, 3, 4]
+
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black rounded-xl p-2 mb-5' >TailwindCss</h1>
+      <Card myName="Chandra" />
+      <Card myName="shekar" btnText="click messs :)" />
+    </>
+  )
+}
+
+export default App
